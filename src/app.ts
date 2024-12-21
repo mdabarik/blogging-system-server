9adc43c3e05ed26957d668e9a5a4c3812ce24204
mdabarik <mdabarik19@gmail.com>
2024-12-21 13:53:20 +0600

    global error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import router from './app/routes';
 
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 // application routes
+app.use('/api/v1', router);
+
+// global routes
 app.get('/', (req: Request, res: Response) => {
   console.log('Home route...');
   res.send({ status: true, message: 'Home page' });
