5d3995bb6fa6c886580098cc085621aa117eeabd
mdabarik <mdabarik19@gmail.com>
2024-12-21 20:42:39 +0600

    auth added

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,7 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+
 export interface TUser {
   name: string;
   email: string;
   password: string;
   role: 'admin' | 'user';
   isBlocked: boolean;
+}
+
+export interface UserModel extends Model<TUser> {
+  //instance methods for checking if the user exist
+  isUserExistsByCustomId(id: string): Promise<TUser>;
+  //instance methods for checking if passwords are matched
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+  isJWTIssuedBeforePasswordChanged(
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ): boolean;
 }
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -51,4 +51,8 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.statics.isUserExistsByCustomId = async function (id: string) {
+  return await User.findOne({ id }).select('+password');
+};
+
 export const User = model<TUser>('User', userSchema);
